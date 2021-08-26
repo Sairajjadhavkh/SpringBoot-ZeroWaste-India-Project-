3964ed665ca546a74619d6ac9591d69438c648cb
Sairajjadhavkh <83511560+Sairajjadhavkh@users.noreply.github.com>
2021-08-27 00:27:52 +0530

    Add files via upload

---
+++ Project WorkSpace/ZeroWaste_India/src/main/java/com/example/demo/ZeroWasteIndiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroWasteIndiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroWasteIndiaApplication.class, args);
+	}
+
+}
